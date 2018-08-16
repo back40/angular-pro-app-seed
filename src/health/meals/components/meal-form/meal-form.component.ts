b3db6e69f99c490d922af025906d0e232a40e0b3
C Conk <back40>
2018-08-16 14:19:37 -0700

    updates for meals

--- src/health/meals/components/meal-form/meal-form.component.ts
+++ src/health/meals/components/meal-form/meal-form.component.ts
@@ -1,14 +1,25 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  OnChanges,
+  SimpleChanges,
+  Input,
+  Output,
+  EventEmitter,
+  ChangeDetectionStrategy
+} from '@angular/core';
 import { FormArray, FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+
 import { Meal } from '../../../shared/services/meals/meals.service';
 
 @Component({
   selector: 'meal-form',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   styleUrls: ['meal-form.component.scss'],
   template: `
     <div class="meal-form">
 
       <form [formGroup]="form">
+
         <div class="meal-form__name">
           <label>
             <h3>Meal name</h3>
@@ -17,10 +28,11 @@ import { Meal } from '../../../shared/services/meals/meals.service';
               placeholder="e.g. English Breakfast"
               formControlName="name">
             <div class="error" *ngIf="required">
-              Meal name is required
+              Workout name is required
             </div>
           </label>
         </div>
+
         <div class="meal-form__food">
           <div class="meal-form__subtitle">
             <h3>Food</h3>
@@ -42,13 +54,22 @@ import { Meal } from '../../../shared/services/meals/meals.service';
             </label>
           </div>
         </div>
+
         <div class="meal-form__submit">
           <div>
             <button
               type="button"
               class="button"
+              *ngIf="!exists"
               (click)="createMeal()">
               Create meal
+            </button>
+            <button
+              type="button"
+              class="button"
+              *ngIf="exists"
+              (click)="updateMeal()">
+              Save
             </button>
             <a
               class="button button--cancel"
@@ -56,13 +77,47 @@ import { Meal } from '../../../shared/services/meals/meals.service';
               Cancel
             </a>
           </div>
+
+          <div class="meal-form__delete" *ngIf="exists">
+            <div *ngIf="toggled">
+              <p>Delete item?</p>
+              <button
+                class="confirm"
+                type="button"
+                (click)="removeMeal()">
+                Yes
+              </button>
+              <button
+                class="cancel"
+                type="button"
+                (click)="toggle()">
+                No
+              </button>
+            </div>
+
+            <button class="button button--delete" type="button" (click)="toggle()">
+              Delete
+            </button>
+          </div>
+
         </div>
+
       </form>
+
     </div>
   `
 })
-export class MealFormComponent {
+export class MealFormComponent implements OnChanges {
+  toggled = false;
+  exists = false;
+
+  @Input() meal: Meal;
+
   @Output() create = new EventEmitter<Meal>();
+
+  @Output() update = new EventEmitter<Meal>();
+
+  @Output() remove = new EventEmitter<Meal>();
 
   form = this.fb.group({
     name: ['', Validators.required],
@@ -71,18 +126,34 @@ export class MealFormComponent {
 
   constructor(private fb: FormBuilder) {}
 
-  get ingredients() {
-    return this.form.get('ingredients') as FormArray;
+  ngOnChanges(changes: SimpleChanges) {
+    if (this.meal && this.meal.name) {
+      this.exists = true;
+      this.emptyIngredients();
+
+      const value = this.meal;
+      this.form.patchValue(value);
+
+      if (value.ingredients) {
+        for (const item of value.ingredients) {
+          this.ingredients.push(new FormControl(item));
+        }
+      }
+    }
+  }
+
+  emptyIngredients() {
+    while (this.ingredients.controls.length) {
+      this.ingredients.removeAt(0);
+    }
   }
 
   get required() {
     return this.form.get('name').hasError('required') && this.form.get('name').touched;
   }
 
-  createMeal() {
-    if (this.form.valid) {
-      this.create.emit(this.form.value);
-    }
+  get ingredients() {
+    return this.form.get('ingredients') as FormArray;
   }
 
   addIngredient() {
@@ -91,5 +162,25 @@ export class MealFormComponent {
 
   removeIngredient(index: number) {
     this.ingredients.removeAt(index);
+  }
+
+  createMeal() {
+    if (this.form.valid) {
+      this.create.emit(this.form.value);
+    }
+  }
+
+  updateMeal() {
+    if (this.form.valid) {
+      this.update.emit(this.form.value);
+    }
+  }
+
+  removeMeal() {
+    this.remove.emit(this.form.value);
+  }
+
+  toggle() {
+    this.toggled = !this.toggled;
   }
 }
